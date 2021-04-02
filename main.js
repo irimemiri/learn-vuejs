1871a06cab132e65762704c403e0f883645771ee
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-03 01:09:04 +0900

    Pass val from parent to child

--- main.js
+++ main.js
@@ -1,60 +1,17 @@
-Vue.filter('localeNum2', function(val){
-    return val.toLocaleString();
+Vue.component('test-comp', {
+    template: '<p>test</p>',
 });
 
-// var myComponent = {
-//     template: '<p>MyComponent</p>'
-// };
-
-Vue.component('my-component', {
-    template: '<p>{{ message }}</p>',
-    data: function() {
-        return {
-            message: 'Hello, Vue.js!'
-        }
+Vue.component('comp-child', {
+    template: '<p>{{ hoge }}</p>',
+    data: function(){
+       return { hoge: 'データはオブジェクトを返す関数じゃないといけないらしい' }
     },
-    methods: {
-        hoge: function() {
-            return 'hoge';
-        }
-    }
+    props: ['val']
 });
 
 var app = new Vue({
     el: '#app',
     data: {
-        list: [],
-        current: '',
-        topics: [
-            { value: 'vue', name: 'Vue.js' },
-            { value: 'jQuery', name: 'jQuery' }
-        ],
-        price: 12800
-    },
-    created: function() {
-    },
-    // components: {
-    //     'my-component': myComponent
-    // },
-    directives: {
-        focus: {
-            inserted: function(el) {
-                el.focus();
-            }
-        }
-    },
-    filters: {
-        localeNum: function(val) {
-            return val.toLocaleString();
-        }
-    },
-    watch: {
-        current: function(val) {
-            axios.get('https://api.github.com/search/repositories', {
-                params: { q: 'topic:' + val }
-            }).then(function(response) {
-                this.list = response.data.items
-            }.bind(this));
-        }
     }
 });
