a963dc02d40a45235675b1249709d91ec40b8cae
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-03 02:13:12 +0900

    Try props-down

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-Vue.component('test-comp', {
-    template: '<p>test</p>',
-});
-
 Vue.component('comp-child', {
-    template: '<p>{{ hoge }}</p>',
-    data: function(){
-       return { hoge: 'データはオブジェクトを返す関数じゃないといけないらしい' }
-    },
-    props: ['val']
+    template: '<li>{{ name }} HP. {{ hp }} <button v-on:click="doAttack">攻撃！</button></li>',
+    props: ['name', 'hp'],
+
+    methods: {
+        doAttack: function() {
+            // 親のリアクティブデータを勝手に更新することはできない(動くけれどwarn)
+            // this.hp -= 10;
+        }
+    }
 });
 
 var app = new Vue({
     el: '#app',
     data: {
+        list: [
+            { id: 1, name: 'スライム', hp: 100 },
+            { id: 2, name: 'ぶちスライム', hp: 200 },
+            { id: 3, name: 'キングスライム', hp: 500 }
+        ]
     }
 });
