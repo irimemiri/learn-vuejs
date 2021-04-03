cd303d73d0dabd9fbaaae0cacfd8ea15918d8d2e
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-03 14:38:21 +0900

    Toggle visiblity of component

--- main.js
+++ main.js
@@ -1,38 +1,17 @@
-Vue.component('slot-test', {
-    template: '<div class="slot-test-comp"><header><slot name="header" text="hello!">ヘッダーデフォルト</slot></header><p><slot>デフォルト</slot></p></div>',
-})
+Vue.component('comp-board', {
+    template: '<div>message board</div>',
+});
 
-Vue.component('comp-child', {
-    template: '<li>{{ name }} HP. {{ hp }} <button v-on:click="doAttack">攻撃！</button></li>',
-    props: {
-        id: Number,
-        name: String,
-        hp: Number
-    },
-    methods: {
-        doAttack: function() {
-            this.$emit('attack', this.id);
-        }
+Vue.component('comp-form', {
+    template: '<div>form<textarea v-model="message"></textarea></div>',
+    data: function() {
+        return { message: '' }
     }
 });
 
 var app = new Vue({
     el: '#app',
     data: {
-        list: [
-            { id: 1, name: 'スライム', hp: 100 },
-            { id: 2, name: 'ぶちスライム', hp: 200 },
-            { id: 3, name: 'キングスライム', hp: 500 }
-        ]
-    },
-    methods: {
-        handleAttack: function(id,e) {
-            // 最初こう書いたけどこれだとダメだった
-            // this.hp -= 10;
-            var item = this.list.find(function(el) {
-                return el.id === id;
-            });
-            if (item !== undefined && item.hp > 0) item.hp -= 10;
-        }
+        current: 'comp-board'
     }
 });
