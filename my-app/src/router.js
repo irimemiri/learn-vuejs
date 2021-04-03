a327c7c0a0b02e1ccff42e2849aaa62752c84caa
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-04 03:47:35 +0900

    Config pattern match routing

--- my-app/src/router.js
+++ my-app/src/router.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import ProductList from '@/views/ProductList.vue'
 import Product from '@/views/Product.vue'
+
 // プラグイン登録
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/', component: Home },
-        { path: '/product', component: Product }
+        { name: 'home', path: '/', component: Home},
+        { name: 'product-list', path: '/product', component: ProductList },
+        { name: 'product', path: '/product/:id', component: Product, props: route => ({ id: Number(route.params.id) }) } // パラメータをpropsとしてコンポーネントに渡す
     ]
 })
 
--- my-app/src/views/Product.vue
+++ my-app/src/views/Product.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="product">
     <h1>商品情報</h1>
+    <p>このページには ID.{{ id }} の商品詳細を表示する</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: { id: Number }
+}
+</script>
